a5d9406bc86b1e818d59158016c749d5dd6c6ee2
Ankur Malyan <malyanankur@gmail.com>
2020-11-05 19:37:23 +0530

    Capability to read the exogenous floorspace

--- cvs/objects/functions/include/building_node_input.h
+++ cvs/objects/functions/include/building_node_input.h
@@ -80,6 +80,8 @@ class SatiationDemandFunction;
  *              - \c base-building-size BuildingNodeInput::mBuildingSize
  *                   The base year size in terms of floorspace which can be utilized to back out
  *					 coefficients.
+ *              - \c is-building-size-fixed BuildingNodeInput::mIsFixedBuildingSize
+ *                   A flag to indicate the user wants to fix the building size to the parsed value
  *              - \c price-exponent BuildingNodeInput::mPriceExponent
  *                   Price exponent by period to be used in calculating a demand for this
  *					 building type.
@@ -377,6 +379,9 @@ protected:
 
         //! Building size by period.
         DEFINE_VARIABLE( ARRAY | STATE, "base-building-size", mBuildingSize, objects::PeriodVector<Value> ),
+
+		//! A flag to indicate the user wants to fix the building size to the parsed value.
+		DEFINE_VARIABLE(ARRAY, "is-building-size-fixed", mIsFixedBuildingSize, objects::PeriodVector<bool>),
 
         //! Price exponent by period.
         DEFINE_VARIABLE( ARRAY, "price-exponent", mPriceExponent, objects::PeriodVector<Value> ),
